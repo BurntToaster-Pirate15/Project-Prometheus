5b5384d18329287bb6382c1d2a98efab0d72260f
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-07-18 19:25:15 +0530

    feat: create a simple dash mechanic

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,15 +1,28 @@
 extends CharacterBody2D
 
 
-const SPEED: float = 100
-
+const SPEED: float = 100.0
+const DASHSPEED: float = 900.0
+var is_dashing: bool = false
 
 
 
 func _physics_process(delta):
+	var speed := SPEED
 	# faster way to fetch direction
 	var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+	
+	
+	if Input.is_action_just_pressed("Dash") && !is_dashing:
+		is_dashing = true
+		$DashTimer.start()
+		speed = DASHSPEED
 
-	velocity =  direction * SPEED 
+	velocity = direction * speed
+
 	# not multiplying by delta because move and slide does it for us
 	move_and_slide()
+
+
+func _on_dash_timer_timeout():
+	is_dashing = false
