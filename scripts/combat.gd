48bb2a0f5a883382f415d55f1c23e4d47e363cf2
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2024-07-21 08:49:21 -1000

    Refactored combat.gd

--- scripts/combat.gd
+++ scripts/combat.gd
@@ -7,16 +7,16 @@ var grid: TileMap
 var current_turn: CharacterBody2D
 var combatants: Array[CharacterBody2D]
 
-
 @onready var player: CharacterBody2D = $"../Player" 
+
 
 func _ready():
 	player = $"../Player"
 	grid = $"../TileMap"
 	
 	for child in get_children():
-		child.connect("body_entered", _on_combat_area_entered.bind(child))
-
+		if(typeof(child)==typeof(Area2D)):
+			child.connect("body_entered", _on_combat_area_entered.bind(child))
 
 
 func _on_combat_area_entered(body: Node2D, area: Area2D):
@@ -25,17 +25,12 @@ func _on_combat_area_entered(body: Node2D, area: Area2D):
 	
 	combatants.append(player)
 	children.map(func(child): if child is CharacterBody2D: combatants.append(child))
-	player.position.x += 32
-	# ^ sets player to the correct position for combat
-	# not an issue
-	# equivalent to setting player to be in some area
-	# where they can do combat
-	in_combat = true # enable combat
+	player.position.x += 32  # TODO: set it to objective position, not relative
+	in_combat = true
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if in_combat and len(combatants) > 1:
-		for combatant in combatants: # Replace with function body.
+		for combatant in combatants:
 			combatant.start_turn(grid, combatants)
 			await combatant.turn_finished
